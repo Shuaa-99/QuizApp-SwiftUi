0cf849eb4e228319b823b4cc01041645f32e8b7f
Shua'a <51505005+Shuaa-99@users.noreply.github.com>
2024-12-28 20:48:58 +0300

    Initial Commit

---
+++ CodeQuizApp/CodeQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeQuizAppApp.swift
+//  CodeQuizApp
+//
+//  Created by SHUAA on 28.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CodeQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
